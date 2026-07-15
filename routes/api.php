aa614d3600d19cb2f893d6c7ab4d2321b0738560
Dorin Roseti <dorin.roseti@gmail.com>
2026-07-15 09:26:42 +0300

    Revert "Initial commit"
    This reverts commit 8e91e743851a18be942a236f90fa03b2c19f8771.

--- routes/api.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\WeatherCheckController;
-
-Route::post('/api/weather-check', [WeatherCheckController::class, '__invoke']);
